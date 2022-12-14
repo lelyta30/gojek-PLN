1eef2dd5637d8724d858e893a776f09f4e04f6e4
NN2777 <naufanafidn@gmail.com>
2022-12-14 10:03:58 +0700

    is security

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -62,7 +62,7 @@ Tip 2: you can also add an image using data-image tag
           @endif
 
           @if (Auth::user()->role == 'CLEANING')
-            <li class="nav-item {{ request()->is('d') ? 'active' : '' }}">
+            <li class="nav-item {{ request()->is('cl') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('cleaning.dashboard') }}">
                     <i class="nc-icon nc-chart-pie-35"></i>
                     <p>Dashboard</p>
@@ -77,6 +77,23 @@ Tip 2: you can also add an image using data-image tag
               </a>
             </li>
           @endif
+
+          @if (Auth::user()->role == 'SECURITY')
+          <li class="nav-item {{ request()->is('sc') ? 'active' : '' }}">
+              <a class="nav-link" href="{{ route('security.dashboard') }}">
+                  <i class="nc-icon nc-chart-pie-35"></i>
+                  <p>Dashboard</p>
+              </a>
+          </li>
+          <hr>
+
+          <li class="nav-item">
+            <a class="nav-link " href="{{ route('security.profile') }}">
+                <i class="nc-icon nc-fav-remove"></i>
+                <p>Profil</p>
+            </a>
+          </li>
+        @endif
           
 
           @if (Auth::user()->role == 'MANAGER')
